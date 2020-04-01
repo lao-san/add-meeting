cb0d707cd3e71186e48db086dab40e3d05ecd111
huwhois <huwhois@163.com>
2020-04-01 16:10:02 +0800

    sign,member-repassword

--- src/views/modules/admin/sign.vue
+++ src/views/modules/admin/sign.vue
@@ -2,12 +2,13 @@
   <div class="mod-config">
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
       <el-form-item>
-        <el-input v-model="dataForm.key" placeholder="账号名或姓名" clearable></el-input>
+        <el-button @click="exportList()">导出名单</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="dataForm.key" placeholder="请输入参数人员/负责人" clearable></el-input>
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
-        <el-button v-if="isAuth('admin:member:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
-        <el-button v-if="isAuth('admin:member:delete')" type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
       </el-form-item>
     </el-form>
     <el-table
@@ -26,13 +27,7 @@
         prop="id"
         header-align="center"
         align="center"
-        label="会员id (会员表)">
-      </el-table-column>
-      <el-table-column
-        prop="username"
-        header-align="center"
-        align="center"
-        label="用户名">
+        label="id">
       </el-table-column>
       <el-table-column
         prop="truename"
@@ -53,48 +48,54 @@
         label="职位">
       </el-table-column>
       <el-table-column
-        prop="jobTitle"
-        header-align="center"
-        align="center"
-        label="职称">
-      </el-table-column>
-      <el-table-column
         prop="phone"
         header-align="center"
         align="center"
-        label="电话">
+        label="联系电话">
       </el-table-column>
       <el-table-column
-        prop="email"
+        prop="typeAttenders"
         header-align="center"
         align="center"
-        label="邮箱">
+        label="参会类型">
       </el-table-column>
       <el-table-column
-        prop="createTime"
+        prop="isPay"
         header-align="center"
         align="center"
-        label="创建时间">
+        label="是否已缴费">
       </el-table-column>
       <el-table-column
-        prop="isCheck"
+        prop="status"
         header-align="center"
         align="center"
-        label="是否通过审核">
-        <template slot-scope="scope">
-          <el-button v-if="scope.row.isCheck === 0" size="medium" hit="true" @click="changeStatus(1,scope.row.id)">否</el-button>
-          <el-button v-else-if="scope.row.isCheck === 1" size="medium"  hit="true" type="success"  @click="changeStatus(0,scope.row.id)">是</el-button>
-        </template>
+        label="签到状态">
+      </el-table-column>
+      <el-table-column
+        prop="badge"
+        header-align="center"
+        align="center"
+        label="胸卡">
+      </el-table-column>
+      <el-table-column
+        prop="servername"
+        header-align="center"
+        align="center"
+        label="负责人">
+      </el-table-column>
+      <el-table-column
+        prop="lastTime"
+        header-align="center"
+        align="center"
+        label="最后签到时间">
       </el-table-column>
       <el-table-column
         fixed="right"
         header-align="center"
         align="center"
-        width="200"
+        width="100"
         label="操作">
         <template slot-scope="scope">
-          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">修改</el-button>
-          <el-button type="text" size="small" @click="repasswordHandle(scope.row.id)">重置密码</el-button>
           <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">删除</el-button>
         </template>
       </el-table-column>
@@ -108,13 +109,10 @@
       :total="totalPage"
       layout="total, sizes, prev, pager, next, jumper">
     </el-pagination>
-    <!-- 弹窗, 新增 / 修改 -->
-    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
   </div>
 </template>
 
 <script>
-  import AddOrUpdate from './member-add-or-update'
   export default {
     data () {
       return {
@@ -122,29 +120,33 @@
           key: ''
         },
         dataList: [],
-        baa: true,
         pageIndex: 1,
         pageSize: 10,
         totalPage: 0,
         dataListLoading: false,
         dataListSelections: [],
-        addOrUpdateVisible: false
+        meetingId: 0
       }
     },
-    components: {
-      AddOrUpdate
+    created () {
+      this.meetingId = this.$route.params.id
     },
     activated () {
       this.getDataList()
     },
     methods: {
+      // 导出数据表
+      exportList () {
+        window.open(this.$http.adornUrl(`/admin/signinfo/downloadxls?token=${this.$cookie.get('token')}&meetingId=${this.meetingId}`))
+      },
       // 获取数据列表
       getDataList () {
         this.dataListLoading = true
         this.$http({
-          url: this.$http.adornUrl('/admin/member/list'),
+          url: this.$http.adornUrl('/admin/signinfo/list'),
           method: 'get',
           params: this.$http.adornParams({
+            'meetingId': this.meetingId,
             'page': this.pageIndex,
             'limit': this.pageSize,
             'key': this.dataForm.key
@@ -175,32 +177,6 @@
       selectionChangeHandle (val) {
         this.dataListSelections = val
       },
-      // 新增 / 修改
-      addOrUpdateHandle (id) {
-        this.addOrUpdateVisible = true
-        this.$nextTick(() => {
-          this.$refs.addOrUpdate.init(id)
-        })
-      },
-      // 重置密码
-      repasswordHandle (id) {
-        this.$http({
-          url: this.$http.adornUrl(`/admin/member/repassword/${id}`),
-          method: 'post',
-          data: this.$http.adornParams()
-        }).then(({data}) => {
-          if (data && data.code === 0) {
-            this.$message({
-              message: '操作成功',
-              type: 'success',
-              duration: 1000,
-              onClose: () => {}
-            })
-          } else {
-            this.$message.error(data.msg)
-          }
-        })
-      },
       // 删除
       deleteHandle (id) {
         var ids = id ? [id] : this.dataListSelections.map(item => {
@@ -212,7 +188,7 @@
           type: 'warning'
         }).then(() => {
           this.$http({
-            url: this.$http.adornUrl('/admin/member/delete'),
+            url: this.$http.adornUrl('/admin/signinfo/delete'),
             method: 'post',
             data: this.$http.adornData(ids, false)
           }).then(({data}) => {
@@ -229,37 +205,6 @@
               this.$message.error(data.msg)
             }
           })
-        })
-      },
-      // 改变审核状态
-      changeStatus (status, id) {
-        this.$http({
-          url: this.$http.adornUrl('/admin/member/status'),
-          method: 'post',
-          data: this.$http.adornData({
-            'id': id,
-            'status': status
-          })
-        }).then(({data}) => {
-          if (data && data.code === 0) {
-            this.$message({
-              message: '操作成功',
-              type: 'success',
-              duration: 500,
-              onClose: () => {
-                // this.getDataList()
-                var dList = this.dataList
-                dList.forEach(element => {
-                  if (element.id === id) {
-                    element.isCheck = status
-                  }
-                })
-                this.dataList = dList
-              }
-            })
-          } else {
-            this.$message.error(data.msg)
-          }
         })
       }
     }
